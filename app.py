39faed56c662708b9de6940eddce184e30575419
jmmestre <mestre.josemario@yahoo.com.br>
2022-06-23 17:42:48 -0300

    Force deploy

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello JMestre V11"
+    return "Hello JMestre V12"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
